cc5a04f38b6c2933dcc9f7690a9448520c461ba2
Yuri Godoz <56451038+yurigodoz@users.noreply.github.com>
2026-02-03 20:36:14 -0300

    Criado variavel de ambiente para swagger

--- backend/src/docs/swagger.js
+++ backend/src/docs/swagger.js
@@ -9,7 +9,7 @@ const swaggerDefinition = {
     },
     servers: [
         {
-        url: 'http://localhost:3000',
+        url: process.env.BASE_URL || 'http://localhost:3000',
         description: 'Servidor local'
         }
     ],
